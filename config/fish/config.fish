f0a7a22df7389b9b08bee20ff74c7309164775c6
Michael Utz <michael@theutz.com>
2022-11-23 13:59:04 +0300

    feat(kitty,fish): turn on deeper shell integration

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,22 @@
 if status is-interactive
+    # starship shell shell-integration
     starship init fish | source
+    # navi shell-integration
     navi widget fish | source
+
+    # zoxide shell-integration
     zoxide init --cmd cd fish | source
 
     # tabtab source for packages
     # uninstall by removing these lines
+    # (important for pnpm)
     [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
+    if set -q KITTY_INSTALLATION_DIR
+        set --global KITTY_SHELL_INTEGRATION enabled
+        source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
+        set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
+    end
 end
 
 # pnpm
