5e641b2433e6a5625337fc01afbc99998be03613
Michael Utz <michael@theutz.com>
2022-11-23 11:35:11 +0300

    feat(fish,pnpm): setup completions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,13 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
     starship init fish | source
     navi widget fish | source
 
+    # tabtab source for packages
+    # uninstall by removing these lines
+    [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 end
 
 # pnpm
-set -gx PNPM_HOME "/Users/michaelutz/Library/pnpm"
+set -gx PNPM_HOME /Users/michaelutz/Library/pnpm
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
