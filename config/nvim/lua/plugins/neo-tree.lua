c74335b9098bd887823dd25f622f67722cc44061
Michael Utz <michael@theutz.com>
2022-11-24 11:05:51 +0300

    feat(nvim,neotree): add window picker

--- config/nvim/lua/plugins/neo-tree.lua
+++ config/nvim/lua/plugins/neo-tree.lua
@@ -1,8 +1,17 @@
 return {
-  cmd = "Neotree",
-  config = function()
-    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+	requires = {
+		{
 
-    require("neo-tree").setup({ filesystem = { follow_current_file = true } })
-  end,
+			"s1n7ax/nvim-window-picker",
+			config = function()
+				require("window-picker").setup()
+			end,
+		},
+	},
+	cmd = "Neotree",
+	config = function()
+		vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+		require("neo-tree").setup({ filesystem = { follow_current_file = true } })
+	end,
 }
