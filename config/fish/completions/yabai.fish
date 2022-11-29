ff43f6e9be1545bb9fbd9f4ebea24762432b945d
Michael Utz <michael@theutz.com>
2022-11-29 22:56:40 +0300

    fix(fish,yabai): improvements

--- config/fish/completions/yabai.fish
+++ config/fish/completions/yabai.fish
@@ -1,28 +1,5 @@
 # Completions for yabai
 
-# ROOT
-# Disable file completion by default
-complete -f -c yabai
-
-# ROOT > <exclusive option>
-complete -x -c yabai -s v -l version \
-    -d "Print the version and exit."
-complete -x -c yabai -s V -l verbose \
-    -d "Output debug information to stdout."
-complete -x -c yabai -l uninstall-sa \
-    -d "Uninstall the scripting-addition. Must be run as root."
-complete -x -c yabai -l load-sa \
-    -d "Load the scripting-addition into Dock.app."
-
-# ROOT > --config
-complete -F -r -c yabai -s c -l config \
-    -d "Use the specified configuration file."
-
-# ROOT > --message
-complete -x -c yabai -s m -l message \
-    -n __fish_is_first_arg \
-    -d "Send message to a running instance of yabai."
-
 function __yabai_echo_argspec
     argparse --min-args=1 --max-args=2 k/keys -- $argv
 
@@ -37,6 +14,39 @@ function __yabai_echo_argspec
     return 0
 end
 
+# `yabai`
+complete -f -c yabai # Disable file completion by default
+
+# `yabai -v`
+# `yabai --version`
+complete -x -c yabai -s v -l version \
+    -n __fish_is_first_arg \
+    -d "Print the version and exit."
+
+# `yabai -V`
+# `yabai --verbose`
+complete -x -c yabai -s V -l verbose \
+    -n __fish_is_first_arg \
+    -d "Output debug information to stdout."
+complete -x -c yabai -l uninstall-sa \
+    -n __fish_is_first_arg \
+    -d "Uninstall the scripting-addition. Must be run as root."
+complete -x -c yabai -l load-sa \
+    -n __fish_is_first_arg \
+    -d "Load the scripting-addition into Dock.app."
+
+# `yabai -c`
+# `yabai --config`
+complete -F -r -c yabai -s c -l config \
+    -n __fish_is_first_arg \
+    -d "Use the specified configuration file."
+
+# `yabai -m`
+# `yabai --message`
+complete -x -c yabai -s m -l message \
+    -n __fish_is_first_arg \
+    -d "Send message to a running instance of yabai."
+
 set -l message_domains '
 config\tGet or set the value of <global setting>
 display\tControl the given display
@@ -47,29 +57,54 @@ rule\tAdd, remove, or list rules
 signal\tReact to some event that has been processed
 '
 
-complete -k -c yabai \
+complete -c yabai \
     -n "__fish_seen_argument -s m -l message" \
     -n "not __fish_seen_subcommand_from (__yabai_echo_argspec -k '$message_domains')" \
     -a "(__yabai_echo_argspec \"$message_domains\")"
 
-# ROOT > --message > config > <setting>
+# `yabai -m config --space <ARG>`
+set -l space_selectors '
+prev\tPrevious space
+next\tNext space
+first\tFirst space
+last\tLast space
+recent\tMost recently visited space
+'
+
+complete -c yabai \
+    -n "__fish_seen_argument -s m -l message" \
+    -n "__fish_seen_argument config" \
+    -n "__fish_prev_arg_in --space" \
+    -a "(__yabai_echo_argspec \"$space_selectors\")" \
+    -d "Get or set the value of <space setting>"
+
+# `yabai -m config [--space=] <ARG>`
 set -l config_settings '
 debug_output\tEnable output of debug information to stdout.
 mouse_follows_focus\tWhen focusing a window, put the mouse at it\'s center.
 '
 
-complete -x -k -c yabai \
+complete -x -c yabai \
     -n "__fish_seen_argument -s m -l message" \
     -n "__fish_seen_subcommand_from config" \
-    -n "not __fish_seen_subcommand_from (__yabai_echo_argspec \"$config_settings\" -k)" \
+    -n "not __fish_seen_argument -l space" \
+    -n "not __fish_seen_subcommand_from (__yabai_echo_argspec -k \"$config_settings\")" \
+    -a "--space\t'Get or set the value of <space setting>'"
+
+complete -x -c yabai \
+    -n "__fish_seen_argument -s m -l message" \
+    -n "__fish_seen_subcommand_from config" \
+    -n "not __fish_prev_arg_in --space" \
+    -n "not __fish_seen_subcommand_from (__yabai_echo_argspec -k \"$config_settings\")" \
     -a "(__yabai_echo_argspec \"$config_settings\")"
 
+# `yabai -m config debug_output <ARG>`
 set -l boolean_values '
 on\tEnable option
 off\tDisable option
 '
 
-complete -x -k -c yabai \
+complete -x -c yabai \
     -n "__fish_seen_argument -s m -l message" \
     -n "__fish_seen_subcommand_from config" \
     -n "__fish_prev_arg_in debug_output" \
