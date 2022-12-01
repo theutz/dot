734a9fda7aa81e1c954848413155cf710d61cc66
Michael Utz <michael@theutz.com>
2022-12-01 12:48:36 +0300

    feat(hs): quake ish thing

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,8 +1,38 @@
+hs.logger.defaultLogLevel = "info"
+local log = hs.logger.new("utz")
+
 hs.loadSpoon("SpoonInstall")
 
 spoon.SpoonInstall:andUse("ReloadConfiguration", { start = true })
-spoon.SpoonInstall:andUse("EmmyLua")
+
+local make_annotations = coroutine.create(function()
+	hs.coroutineApplicationYield()
+	spoon.SpoonInstall:andUse("EmmyLua")
+	log:i("Hammerspoon annotations written")
+end)
 
 require("utzwm")
 
-hs.alert.show("Hammerspoon Loaded!")
+hs.hotkey.bind({ "cmd" }, "g", function()
+	local app = hs.application.get("kitty")
+	if app then
+		if not app:mainWindow() then
+			app:selectMenuItem({ "Shell", "New OS Window" })
+		elseif app:isFrontmost() then
+			app:hide()
+		else
+			app:activate()
+		end
+	else
+		hs.application.launchOrFocus("kitty")
+		app = hs.application.get("kitty")
+	end
+
+	app:mainWindow():moveToUnit("100,50,0,0")
+	app:mainWindow().setShadows(false)
+end)
+
+log:i("Hammerspoon Reloaded")
+hs.notify.show("Hammerspoon", "", "Config reloaded")
+
+coroutine.resume(make_annotations)
