63bb00d27d2b2c4fb5d8d44e9f5c8a780b294da3
Michael Utz <michael@theutz.com>
2022-12-05 13:28:51 +0300

    feat(nvim): more luasnip like tj

--- config/nvim/lua/plugins/luasnip.lua
+++ config/nvim/lua/plugins/luasnip.lua
@@ -30,21 +30,39 @@ function M.config()
 		return luasnip.jump(-1) and "" or util.t("<S-Tab>")
 	end
 
+	vim.keymap.set({ "i", "s" }, "<c-k>", function()
+		if luasnip.expand_or_jumpable() then
+			luasnip.expand_or_jump()
+		end
+	end, { silent = true })
+
+	vim.keymap.set({ "i", "s" }, "<c-j>", function()
+		if luasnip.jumpable(-1) then
+			luasnip.jump(-1)
+		end
+	end, { silent = true })
+
+	vim.keymap.set("i", "<c-l>", function()
+		if luasnip.choice_active() then
+			luasnip.change_choice(1)
+		end
+	end)
+
 	-- vim.keymap.set("i", "<Tab>", on_tab, { expr = true })
 	-- vim.keymap.set("s", "<Tab>", on_tab, { expr = true })
 	-- vim.keymap.set("i", "<S-Tab>", on_s_tab, { expr = true })
 	-- vim.keymap.set("s", "<S-Tab>", on_s_tab, { expr = true })
 
-	vim.cmd([[
-" press <Tab> to expand or jump in a snippet. These can also be mapped separately
-" via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
-imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
-" -1 for jumping backwards.
-inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
-
-snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
-snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
-]])
+	-- 	vim.cmd([[
+	-- " press <Tab> to expand or jump in a snippet. These can also be mapped separately
+	-- " via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
+	-- imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
+	-- " -1 for jumping backwards.
+	-- inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
+	--
+	-- snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
+	-- snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
+	-- ]])
 end
 
 return M
