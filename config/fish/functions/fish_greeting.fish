6ad56fb50bd7ad8c78410eb6c1f6affc73ef9518
Michael Utz <michael@theutz.com>
2022-12-07 23:28:25 +0300

    feat(neofetch): do neofetch sometimes

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,4 +1,11 @@
 function fish_greeting
+    set -l greeting neofetch figlet
     set -l fonts isometric1 isometric2 isometric3 isometric4
-    figlet -w120 -f (random choice $fonts) TheUtz | lolcat
+
+    switch (random choice $greeting)
+        case neofetch
+            neofetch
+        case figlet
+            figlet -w120 -f (random choice $fonts) TheUtz | lolcat
+    end
 end
