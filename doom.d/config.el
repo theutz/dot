91a251a97e6db8c24ca2854a04131e0f187ad83e
Michael Utz <michael@theutz.com>
2022-12-14 02:39:16 +0300

    feat(doom): don't use variable pitch

--- doom.d/config.el
+++ doom.d/config.el
@@ -22,7 +22,7 @@
 ;; accept. For example:
 ;;
 (setq doom-font (font-spec :family "Haskplex Nerd" :size 14 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "Chubbo" :size 14))
+      doom-variable-pitch-font (font-spec :family "VictorMono Nerd Font Mono" :size 16))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
