c6bf7de7327a6e3c0575642e4a530af00fcfeeeb
Michael Utz <michael@theutz.com>
2022-12-16 22:30:26 +0300

    feat(doom): capture template for bookmarks

--- doom.d/config.el
+++ doom.d/config.el
@@ -84,6 +84,9 @@
   (add-hook 'org-mode-hook
             #'mixed-pitch-mode)
   (custom-set-faces!
-    '(org-document-title :height 1.3)))
+    '(org-document-title :height 1.3))
+  (push '("b" "Bookmark (Clipboard)" entry (file+headline "bookmarks.org" "Bookmarks")
+          "** %(org-cliplink-capture)\n:PROPERTIES:\n:TIMESTAMP: %t\n:END:%?\n" :empty-lines 1 :prepend t) org-capture-templates))
+
 
 (setq-default line-spacing 6)
