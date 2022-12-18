7e1075294933771db0aedbdcb779d00ee0934945
Michael Utz <michael@theutz.com>
2022-12-19 01:37:29 +0300

    feat(doom): get email working!

--- doom.d/config.el
+++ doom.d/config.el
@@ -100,3 +100,14 @@
 
 (map! :localleader :mode 'org-mode :n "d =" 'org-timestamp-up)
 (map! :localleader :mode 'org-mode :n "d -" 'org-timestamp-down)
+
+(add-to-list 'auto-mode-alist '("/authinfo\\.gpg\\'" . authinfo-mode))
+
+(setq auth-sources '("~/.authinfo.gpg"))
+
+(after! mu4e
+  (setq sendmail-program (executable-find "msmtp")
+        send-mail-function #'smtpmail-send-it
+        message-sendmail-f-is-evil t
+        message-sendmail-extra-arguments '("--read-envelope-from")
+        message-send-mail-function #'message-send-mail-with-sendmail))
