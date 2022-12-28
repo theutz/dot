38b4b5f0774225d1094238e08bdccd10d343b297
Michael Utz <michael@theutz.com>
2022-12-28 12:45:16 +0300

    feat(doom): don't advice about LSP nonsense

--- doom.d/custom.el
+++ doom.d/custom.el
@@ -3,10 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values '((org-duration-format . h:mm))))
+ '(safe-local-variable-values '((org-duration-format . h:mm)))
+ '(warning-suppress-types '((lsp-mode) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ts-fold-replacement-face ((t (:foreground nil :box nil :inherit font-lock-comment-face :weight light)))))
