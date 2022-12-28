a4a8310f2999602ccc8a7c37a2d90eb084ee4ee0
Michael Utz <michael@theutz.com>
2022-12-28 10:23:34 +0300

    feat(hs): cleanup hyper

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -13,18 +13,25 @@ spoon.SpoonInstall:andUse("ReloadConfiguration", {
 local hyper = require('hyper')
 require("utzwm")
 
-hyper.bindApp({}, "b", "Vivaldi")
-hyper.bindApp({}, "e", "Emacs")
-hyper.bindApp({}, "k", "kitty")
+hyper.bindApp({}, "1", "Vivaldi")
+hyper.bindApp({}, "2", "Emacs")
+hyper.bindApp({}, "3", "kitty")
+hyper.bindApp({}, "4", "Spotify")
+hyper.bindApp({}, "5", "Surfshark")
+
+hyper.bindApp({}, "c", "Google Chrome")
 hyper.bindApp({}, "f", "Figma")
-hyper.bindApp({}, "p", "Spotify")
+hyper.bindApp({}, "m", "Messages")
 hyper.bindApp({}, "s", "Slack")
 hyper.bindApp({}, "t", "Telegram")
 hyper.bindApp({}, "w", "WhatsApp")
-hyper.bindApp({}, "m", "Messages")
 
 hs.hotkey.bindSpec({ mods, "c" }, function()
-  hs.task.new("/opt/homebrew/bin/fish", nil, { "-l", "-c", "doom +eveywhere" }):start()
+  hs.task.new("/opt/homebrew/bin/fish", function(exitCode, stdErr)
+    if exitCode > 0 then
+      print('Emacs Everywhere: ', exitCode, stdErr)
+    end
+  end, { "-l", "-c", 'doom +everywhere' }):start()
 end)
 
 hs.alert.show("Hammerspoon Reloaded!")
