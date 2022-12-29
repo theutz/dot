7a9dc4c9c45d3019551f0988e60a3971bf655e72
Michael Utz <michael@theutz.com>
2022-12-29 15:06:22 +0300

    feat(hs): use numbers and letters for apps

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -13,20 +13,25 @@ spoon.SpoonInstall:andUse("ReloadConfiguration", {
 local hyper = require('hyper')
 require("utzwm")
 
-hyper.bindApp({}, "1", "Vivaldi")
-hyper.bindApp({}, "2", "Emacs")
-hyper.bindApp({}, "3", "kitty")
-hyper.bindApp({}, "4", "Spotify")
-hyper.bindApp({}, "5", "Surfshark")
+hyper.bindApp({}, "1", "Emacs") -- Default Editor
+hyper.bindApp({}, "2", "Vivaldi") -- Default Browser
+hyper.bindApp({}, "3", "kitty") -- Default Terminal
+hyper.bindApp({}, "4", "Spotify") -- Default Music Player
+hyper.bindApp({}, "5", "Surfshark") -- Default VPN
 
 hyper.bindApp({}, "c", "Google Chrome")
 hyper.bindApp({}, "d", "Dash")
+hyper.bindApp({}, "e", "Emacs")
 hyper.bindApp({}, "f", "Figma")
 hyper.bindApp({}, "h", "Hammerspoon")
+hyper.bindApp({}, "k", "kitty")
 hyper.bindApp({}, "m", "Messages")
+hyper.bindApp({}, "p", "Spotify")
 hyper.bindApp({}, "r", "Ray")
 hyper.bindApp({}, "s", "Slack")
 hyper.bindApp({}, "t", "Telegram")
+hyper.bindApp({}, "u", "Surfshark")
+hyper.bindApp({}, "v", "Vivaldi")
 hyper.bindApp({}, "w", "WhatsApp")
 
 local function logTaskErr(taskName)
