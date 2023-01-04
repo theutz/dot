fcbc5d5bd28077d024ea5d0101f4ea4466b135ea
Michael Utz <michael@theutz.com>
2023-01-04 11:26:50 +0300

    feat(doom): custom

--- doom.d/custom.el
+++ doom.d/custom.el
@@ -4,13 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((gac-automatically-add-new-files-p t)
+   '((projectile-project-test-cmd . "php artisan test")
      (org-duration-format . h:mm)
-     (gac-automatically-push-p t)))
- '(warning-suppress-types '((lsp-mode) (defvaralias))))
+     (gac-automatically-add-new-files-p t)
+     (gac-automatically-push-p t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ts-fold-replacement-face ((t (:foreground nil :box nil :inherit font-lock-comment-face :weight light)))))
+ )
