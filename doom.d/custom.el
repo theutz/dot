76d41fc0a19e42729df6a15db3eaaf0ce8bd2864
Michael Utz <michael@theutz.com>
2023-01-11 21:31:46 +0300

    fix(doom): custom

--- doom.d/custom.el
+++ doom.d/custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((lsp-intelephense-php-version . "8.2")
+   '((phpunit-executable "vendor/bin/pest")
+     (lsp-intelephense-php-version . "8.2")
      (projectile-project-test-cmd . "php artisan test")
      (org-duration-format . h:mm)
      (gac-automatically-add-new-files-p t))))
@@ -13,4 +14,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ts-fold-replacement-face ((t (:foreground nil :box nil :inherit font-lock-comment-face :weight light)))))
